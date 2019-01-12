1d12c69a9fd945e73945e421e89989462acef8b9
neocryses <neocryses@users.noreply.github.com>
2019-01-13 02:12:58 +0900

    Update

--- init.el
+++ init.el
@@ -261,22 +261,22 @@ Fundamental-mode, and disable the undo"
          (jp-font-family "Ricty Discord"))
     (set-face-attribute 'default nil :family font-family :height font-height)
     (let ((name (frame-parameter nil 'font))
+          (font-spec (font-spec :family font-family))
+          (characters '((?\u00A0 . ?\u00FF) ; Latin-1
+                        (?\u0100 . ?\u017F) ; Latin Extended-A
+                        (?\u0180 . ?\u024F) ; Latin Extended-B
+                        (?\u0250 . ?\u02AF) ; IPA Extensions
+                        (?\u0370 . ?\u03FF))) ; Greek and Coptic
           (jp-font-spec (font-spec :family jp-font-family))
           (jp-characters '(katakana-jisx0201
                            cp932-2-byte
                            japanese-jisx0212
                            japanese-jisx0213-2
-                           japanese-jisx0213.2004-1))
-          (font-spec (font-spec :family font-family))
-          (characters '((?\u00A0 . ?\u00FF)    ; Latin-1
-                        (?\u0100 . ?\u017F)    ; Latin Extended-A
-                        (?\u0180 . ?\u024F)    ; Latin Extended-B
-                        (?\u0250 . ?\u02AF)    ; IPA Extensions
-                        (?\u0370 . ?\u03FF)))) ; Greek and Coptic
-      (dolist (jp-character jp-characters)
-        (set-fontset-font name jp-character jp-font-spec))
+                           japanese-jisx0213.2004-1))) 
       (dolist (character characters)
         (set-fontset-font name character font-spec))
+      (dolist (jp-character jp-characters)
+        (set-fontset-font name jp-character jp-font-spec))
       (add-to-list 'face-font-rescale-alist (cons jp-font-family 1.3)))))
 (set-font)
 (add-hook 'after-make-frame-functions #'set-font)
