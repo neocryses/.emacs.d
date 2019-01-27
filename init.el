beac5f1ca586cf583d5b3d90b396d225bb78fa91
neocryses <neocryses@users.noreply.github.com>
2019-01-28 01:32:57 +0900

    Update

--- init.el
+++ init.el
@@ -354,17 +354,18 @@ _l_: move right  _L_: move window right _+_: Increase height
                           (?\u0100 . ?\u017F) ; Latin Extended-A
                           (?\u0180 . ?\u024F) ; Latin Extended-B
                           (?\u0250 . ?\u02AF) ; IPA Extensions
-                          (?\u0370 . ?\u03FF))) ; Greek and Coptic
+                          (?\u0370 . ?\u03FF) ; Greek and Coptic
+                          (?\u0183 . ?\u0183))) ;Whitespace-mode ·
             (ja-font-spec (font-spec :family ja-font-family))
             (ja-characters '(katakana-jisx0201
                              cp932-2-byte
                              japanese-jisx0212
                              japanese-jisx0213-2
                              japanese-jisx0213.2004-1))) 
-        (dolist (character characters)
-          (set-fontset-font name character font-spec))
         (dolist (ja-character ja-characters)
           (set-fontset-font name ja-character ja-font-spec))
+        (dolist (character characters)
+          (set-fontset-font name character font-spec))
         (add-to-list 'face-font-rescale-alist (cons ja-font-family ja-default-font-rescale))))))
 (add-hook 'after-init-hook #'set-font)
 (add-hook 'after-make-frame-functions #'set-font)
@@ -481,6 +482,50 @@ _l_: move right  _L_: move window right _+_: Increase height
   (tool-bar-mode -1))
 (when (boundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
+
+;;;; Remember window size
+
+(defun save-framegeometry ()
+  "Gets the current frame's geometry and saves to ~/.emacs.d/framegeometry."
+  (let ((framegeometry-left (frame-parameter (selected-frame) 'left))
+        (framegeometry-top (frame-parameter (selected-frame) 'top))
+        (framegeometry-width (frame-parameter (selected-frame) 'width))
+        (framegeometry-height (frame-parameter (selected-frame) 'height))
+        (framegeometry-file (expand-file-name "~/.emacs.d/framegeometry")))
+
+    (when (not (number-or-marker-p framegeometry-left))
+      (setq framegeometry-left 0))
+    (when (not (number-or-marker-p framegeometry-top))
+      (setq framegeometry-top 0))
+    (when (not (number-or-marker-p framegeometry-width))
+      (setq framegeometry-width 0))
+    (when (not (number-or-marker-p framegeometry-height))
+      (setq framegeometry-height 0))
+
+    (with-temp-buffer
+      (insert
+       ";;; This is the previous emacs frame's geometry.\n"
+       ";;; Last generated " (current-time-string) ".\n"
+       "(setq initial-frame-alist\n"
+       "      '(\n"
+       (format "        (top . %d)\n" (max framegeometry-top 0))
+       (format "        (left . %d)\n" (max framegeometry-left 0))
+       (format "        (width . %d)\n" (max framegeometry-width 0))
+       (format "        (height . %d)))\n" (max framegeometry-height 0)))
+      (when (file-writable-p framegeometry-file)
+        (write-file framegeometry-file)))))
+
+(defun load-framegeometry ()
+  "Loads ~/.emacs.d/framegeometry which should load the previous frame's geometry."
+  (let ((framegeometry-file (expand-file-name "~/.emacs.d/framegeometry")))
+    (when (file-readable-p framegeometry-file)
+      (load-file framegeometry-file))))
+
+;; Special work to do ONLY when there is a window system being used
+(if window-system
+    (progn
+      (add-hook 'after-init-hook 'load-framegeometry)
+      (add-hook 'kill-emacs-hook 'save-framegeometry)))
 
 ;;;; Themes
 
